114e5914d811616f0ef9b50556c4b9e54bab07cf
Jordan Dowling <jdowling89@gmail.com>
2025-06-14 15:15:05 -0400

    Update mlb_team_scrape.py
    update code for team hitting stats - complete

--- mlb_team_scrape.py
+++ mlb_team_scrape.py
@@ -3,8 +3,10 @@ from bs4 import BeautifulSoup
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import pandas as pd
 import numpy as np
+import re
 
 #function to scrape website with URL param
 #returns parsed html
@@ -15,33 +17,56 @@ def get_soup(URL):
 
     driver = webdriver.Chrome(options=options)
     driver.get(URL)
+    
     #get page source
-    html = driver.page_source
+    html = driver.execute_script("return document.documentElement.outerHTML;")
     #close driver for webpage
+    driver.implicitly_wait(3)
+    #stats_table = driver.find_element(By.CSS_SELECTOR, "bui-table")
+    #print(stats_table)
+
     driver.quit
     soup = BeautifulSoup(html, 'html.parser')
     return soup
 
-def get_stats(soup):
-    stats_table = soup.find('div', attr={"class":"stats-body-table team"})
-    #get column headers from mlb table
-    headers = soup.findAll('div', class_='bui-text cellheader bui-text')
-    return headers
+def get_stats(URL):
+    #get parsed data from each url
+    soup_hitting = get_soup(hitting_stats_url)
+
+    #parse tables off url, returns list of dictionaries (list of tables from url)
+    df_hitting_list = pd.read_html(hitting_stats_url)
+
+    #create dataframe from 1st list in df_hitting_list
+    hitting_df = df_hitting_list[0]
+
+    #update column names to remove dupe (EX: TEAMTEAM)
+    hitting_df.columns = ['TEAM', 'LEAGUE', 'G', 'AB', 'R', 'H', '2B', '3B', 'HR', 'RBI', 'BB', 'SO', 'SB', 'CS', 'AVG', 'OBP', 'SLG', 'OPS']
+
+    #remove numbers from team name in 1st column and remove dupe team name from every col0 on every row 
+    # (ex: New York YankeesYankees -> New York Yankees)
+    for index, name in hitting_df.iterrows():
+        hitting_df.iloc[index, 0] = re.sub(r'\d+', '', hitting_df.iloc[index,0])
+        team_name = " ".join(re.split(r'(?=[A-Z])', hitting_df.iloc[index, 0])[:-1])
+        #team_name = ' '.join(team_name.split()[:-1]
+        hitting_df.iloc[index, 0] = team_name
+    print(hitting_df)
+
+    #output to xlsx
+    hitting_df.to_excel('Team Hitting Stats.xlsx', sheet_name='2025')
+
+
 
 #url for each team standings, add year at the end of url string to get particular year
-standings_url = 'https://www.mlb.com/standings/'
-
+standings_url = 'https://www.mlb.com/standings/' 
 #url for season hitting stats for all teams, add year at end of url for particular year
 hitting_stats_url = 'https://www.mlb.com/stats/team'
-
 #url for season pitching stats for all teams, add year at end of url for particular year
 pitching_stats_url = 'https://www.mlb.com/stats/team/pitching'
 
-#get parsed data from each url
-soup_hitting = get_soup(hitting_stats_url)
+get_stats(hitting_stats_url)
+
+
+
 #soup_pitching = get_soup(pitching_stats_url)
 #soup_standings = get_soup(standings_url)
 
-#get data from 
-team_hit_stats = get_stats(soup_hitting)
-print(team_hit_stats)
